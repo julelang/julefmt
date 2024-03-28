31c251883a0a70d3fcbe11558cbc3098098e134e
mertcandav <mertcandav@outlook.com>
2024-03-28 18:00:59 +0300

    add build scripts

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2024 The Jule Programming Language.
+# Use of this source code is governed by a BSD 3-Clause
+# license that can be found in the LICENSE file.
+
+if [ -f ./src/main.jule ]; then
+  julec --opt-deadcode -o julefmt ./src
+else
+  echo "error: working directory is not root directory"
+  exit
+fi
+
+if [ $? -eq 0 ]; then
+  echo "Compilation successful!"
+else
+  echo "-----------------------------------------------------------------------"
+  echo "An unexpected error occurred while compiling JuleFmt. Check errors above."
+fi
